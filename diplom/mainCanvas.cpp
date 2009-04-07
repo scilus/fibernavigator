2f4d9706b3e1cb5775abb9a8f6bc6447e0b26e05
Mario Hlawitschka <hlawitschka@googlemail.com>
2009-04-07 18:14:59 +0000

    * do not check for GLX on OSX because we do not use X there and, therefore, the function does not exist

--- diplom/mainCanvas.cpp
+++ diplom/mainCanvas.cpp
@@ -28,6 +28,7 @@ MainCanvas::MainCanvas(DatasetHelper* dh, int view, wxWindow *parent, wxWindowID
     : wxGLCanvas(parent, shared, id, pos, size, style|wxFULL_REPAINT_ON_RESIZE, name, gl_attrib )
 #endif
 {
+#ifndef __WXMAC__
 	// i'm still not sure which GLX version is needed, but 1.3 seems to be ok
 	// have to do this here, because wxGLCanvas::GetGLXVersion doesn't return reliable results when called 
 	// in the init function in main.cpp
@@ -37,7 +38,7 @@ MainCanvas::MainCanvas(DatasetHelper* dh, int view, wxWindow *parent, wxWindowID
 		printf("OpenGL and a sufficient graphics card need to be installed to run this programm.\n");
 		exit(false);
 	}
-	
+#endif
 	m_init = false;
 	m_view = view;
 	m_dh = dh;
