e9840fbf7a97453ffa12573fb410b8c2bc378e4f
Ralph Schurade <schurade@gmail.com>
2009-06-15 12:30:23 +0000

    [FIX] add alpha blending to drawing vectors for smooth effect

--- diplom/gfx/theScene.cpp
+++ diplom/gfx/theScene.cpp
@@ -549,6 +549,9 @@ void TheScene::drawVectors()
 {
     glPushAttrib(GL_ALL_ATTRIB_BITS);
 
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     glEnable( GL_LINE_SMOOTH);
     glHint( GL_LINE_SMOOTH_HINT, GL_NICEST);
 
