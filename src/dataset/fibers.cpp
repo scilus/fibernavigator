1f6a3c2e0ae6901a35e8982b88c8c4d301adf03e
Arnaud Boré <arnaud.bore@gmail.com>
2011-08-04 19:21:20 +0000

    modification mrtrix loader

--- src/dataset/fibers.cpp
+++ src/dataset/fibers.cpp
@@ -728,11 +728,11 @@ bool Fibers::loadMRtrix( wxString i_filename )
 
     for ( int i = 0; i < m_countPoints * 3; ++i )
     {
-        m_pointArray[i] = m_pointArray[i] + 0.5 + m_dh->m_columns/2;
+        m_pointArray[i] = m_pointArray[i] + 0.5 + (m_dh->m_columns/2)*m_dh->m_xVoxel;
         ++i;
-        m_pointArray[i] = m_pointArray[i] + 0.5 + m_dh->m_rows/2;
+		m_pointArray[i] = m_pointArray[i] + 0.5 + (m_dh->m_rows/2)*m_dh->m_yVoxel ;
         ++i;
-        m_pointArray[i] = m_pointArray[i] + 0.5 + m_dh->m_frames/2;
+        m_pointArray[i] = m_pointArray[i] + 0.5 + (m_dh->m_frames/2)*m_dh->m_zVoxel;
     }
 
     m_dh->printDebug(wxT("End loading TCK file"), 1 );
