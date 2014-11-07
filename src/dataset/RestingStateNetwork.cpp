963c87f30a3d0f392cc8e89d33112bd0c03a0a78
Maxime C <maxime.chamberland@usherbrooke.ca>
2014-11-07 09:42:43 +0200

    Fixed glPushAtrib that was called too often and made the rtfmri lag when no thresholds

--- src/dataset/RestingStateNetwork.cpp
+++ src/dataset/RestingStateNetwork.cpp
@@ -413,7 +413,7 @@ void RestingStateNetwork::render3D(bool recalculateTexture)
 	if( m_3Dpoints.size() > 0 )
     {
 		std::vector<float> texture(m_datasetSizeL*3, 0.0f);
-
+        glPushAttrib( GL_ALL_ATTRIB_BITS );
 
         ////Test Sort////
         GLfloat projMatrix[16];
@@ -471,7 +471,7 @@ void RestingStateNetwork::render3D(bool recalculateTexture)
 				B = v;
 			}
 
-			glPushAttrib( GL_ALL_ATTRIB_BITS );
+			
 			glEnable(GL_BLEND);
 			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 			glEnable(GL_POINT_SPRITE);
@@ -495,7 +495,7 @@ void RestingStateNetwork::render3D(bool recalculateTexture)
 			//glDisable( GL_TEXTURE_2D );
 			glDisable(GL_POINT_SPRITE);
 			glDisable(GL_BLEND);
-			glPopAttrib();
+			
 
 			int zz = ((m_3Dpoints[s].first.z - m_originL.z) * m_zL / m_voxelSizeZ) + m_origin.z;
 				int yy = ((m_3Dpoints[s].first.y - m_originL.y) * m_yL / m_voxelSizeY) + m_origin.y;
@@ -543,6 +543,7 @@ void RestingStateNetwork::render3D(bool recalculateTexture)
 
 		//}
         delete[] pSnippletSort;
+        glPopAttrib();
 	}
 }
 
